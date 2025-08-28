84b86e46af92bf102d1dff01714264239cefc4b9
Danthecalfman <spikeeboy@outlook.com>
2025-08-28 16:06:29 -0700

    eq overload

--- card.py
+++ card.py
@@ -7,5 +7,15 @@ class Card():
     def display(self):
         print(f"{self.rank}{self.suit}", end = " ")
 
+    def __eq__(self, other):
+        if isinstance(other, Card):
+            if (self.rank == other.rank):
+                return True
+        elif isinstance(other, str):
+            if (self.rank == other):
+                return True
+        return False
+    
+    
     def __add__(self, value):
         
