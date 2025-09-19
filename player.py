51d8f8be5b4c54789d904a66e06ba7df60a6ff10
Danthecalfman <spikeeboy@outlook.com>
2025-09-19 10:43:29 -0700

    Update player.py

--- player.py
+++ player.py
@@ -4,10 +4,10 @@ from deck import *
 from table import *
 
 class Player():
-    def __init__(self, name):
+    def __init__(self, name, table):
         self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         self.hand = [] 
-        self.table = Table()
+        self.table = table
         self.name = name
         self.chips = 0
         self.bet = 0
